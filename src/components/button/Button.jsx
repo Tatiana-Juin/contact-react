f3580979f2d04caf4197d3e7eb4b0e5746993de3
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-03-24 11:12:38 +0100

    Implementation de la structure du composant Button de Header

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button(){
+
+     return(
+        <>
+            <p>Bouton</p>
+        </>
+     )
+}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
+import Button from "../button/Button"
 export default function Header(){
 
 
     return(
         <>
             {/* <p>Header</p> */}
+            {/* <Button /> */}
         </>
     )
 }
