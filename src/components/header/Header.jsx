8961a16f5753f28facbf6e22d209c58c91a93165
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-03-24 11:31:08 +0100

    Debut du style du composant Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,13 @@
+import fstyle from './header.module.css'
 import Button from "../button/Button"
 export default function Header(){
 
 
     return(
         <>
-            
+            <div className={fstyle.header}>
+                    
+            </div>
         </>
     )
 }
