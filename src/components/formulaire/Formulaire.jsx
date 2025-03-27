400816d1778528c3ed629a41613a53d923ffd66e
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-03-27 11:22:12 +0100

    J'ai commencer a faire le formulaire et a ajouter l'image .

--- src/components/formulaire/Formulaire.jsx
+++ src/components/formulaire/Formulaire.jsx
@@ -1,8 +1,48 @@
+import Image from '../../assets/image/home.svg'
+import fstyle from './formulaire.module.css'
+
+import Button from '../button/Button'
 export default function Formulaire(){
 
     return(
         <>
-            {/* <p>Formulaire</p> */}
+            <div className={fstyle.formulaire}>
+                
+                <h2>CONTACTER NOUS PAR MAIL</h2>
+                
+                {/* DIV REGROUPANT TOUS   */}
+
+                <div>
+                    {/* FORMULAIRE DE CONTACT */}
+                            <form action="" method="post">
+                                {/* nom */}
+                                <label htmlFor="nom">Nom</label>
+                                <input type="text" name="nom" id="nom" />
+                                
+                                {/* prenom */}
+                                <label htmlFor="prenom">Prénom</label>
+                                <input type="text" name="prenom" id="prenom" />
+
+                                {/* email*/}
+                                <label htmlFor="email">Email</label>
+                                <input type="email" name="email" id="email" />
+
+                                {/* Message */}
+                                <label htmlFor="message">Message</label>
+                                <textarea name="message" id="message"></textarea>
+
+                                <Button text="ENVOYER" />
+
+                            </form>
+                      
+
+                     
+                           <img src={Image} alt="Image d'une enveloppe " style={{width:"500px"}} />
+                       
+
+                </div>
+                
+            </div>
         </>
     )
 }
